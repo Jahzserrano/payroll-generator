3e8841af481679acc4eabfc5cb67e9236dbcecfe
jahzserrano <serranojahaziel2@gmail.com>
2023-10-02 13:44:57 -0400

    First commit. added .xlsx file and main file nomigen.py. added openpyxl library and connected the library with the .xlsx file

---
+++ nomigen.py
@@ -0,0 +1,18 @@
+
+# Bermudez Serrano, Jahaziel A.
+# Description: Script that generates a two weeks payroll
+# -----------------------------------------------------------
+
+
+# import Openpyxl 
+from openpyxl import load_workbook
+
+# Connect with existing file
+wb = load_workbook(filename="Plantilla_Record_For_Payroll.xlsx")
+
+
+print(wb.sheetnames)
+
+
+# TODO: assign values to cells
+
